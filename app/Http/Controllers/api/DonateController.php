6daf29fc3cd00bc7e6299b0ff789b90e3a348cf8
DiegoEmanuel <diegoabox@gmail.com>
2021-06-29 17:40:19 -0300

    first commit

---
+++ app/Http/Controllers/api/DonateController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\api;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Donate;
+
+
+class DonateController extends Controller
+{
+    public function index()
+    {
+        return Donate::all();
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
